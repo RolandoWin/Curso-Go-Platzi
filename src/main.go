86b7ea29db9f0700da44563731a472f3a27b8b51
Rolando <rolandolazaro@gmail.com>
2022-08-25 11:53:54 -0500

    Uso de constantes y variables

--- src/main.go
+++ src/main.go
@@ -3,7 +3,28 @@ package main
 import "fmt"
 
 func main() {
+	// Declaración de constantes
+	const pi float64 = 3.1416
+	const pi2 = 3.141592
+	fmt.Println(pi, pi2)
+	//Declaración de variables enteras
+	base := 12
+	var altura int = 14
+	var area int
 
-	fmt.Println("Hola Mundo!!!")
+	fmt.Println(base, altura, area)
+
+	var a int
+	var b float64
+	var c string
+	var d bool
+
+	fmt.Println(a, b, c, d)
+
+	//Aréa cuadrado
+	const baseCuadrado = 10
+	var areaCuadrado int = baseCuadrado * baseCuadrado
+
+	fmt.Println("Aréa cuadrado: ", areaCuadrado)
 
 }
