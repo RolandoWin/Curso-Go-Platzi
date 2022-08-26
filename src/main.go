33d3183ace51a7fe98b1a3950e9b509be5409644
Rolando <rolandolazaro@gmail.com>
2022-08-25 21:23:08 -0500

    Agregar Funciones Go

--- src/main.go
+++ src/main.go
@@ -1,6 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
+
+func normalFunction(message string) {
+	fmt.Println(message)
+}
+
+func tripleArgumento(a, b int, c string) {
+	fmt.Println(a, b, c)
+}
+
+func returnValue(a int) int {
+	return a * 2
+}
+
+func doubleReturn(a float64) (c float64, d float64) {
+	return a, (math.Pi * a)
+}
 
 func main() {
 	// Declaración de constantes
@@ -70,5 +89,35 @@ func main() {
 	var radio float64 = 10
 	var areaCirculo float64 = pi * (radio * radio)
 	fmt.Println("Aréa del circulo: ", areaCirculo)
+
+	helloMessage := "Hello"
+	worldMessage := "World"
+
+	//Println
+	fmt.Println(helloMessage, worldMessage)
+
+	//Printf
+	nombre := "Platzi"
+	cursos := 500
+	fmt.Printf("%s tiene más de %d\n", nombre, cursos)
+	//si no sabemos el tipo de la variable
+	fmt.Printf("%v tiene más de %v\n", nombre, cursos)
+
+	//Sprintf: No imprime en consola solo lo guarda en memoria
+	message := fmt.Sprintf("%v tiene más de %v", nombre, cursos)
+	fmt.Print(message, "\n")
+
+	//Tipo de datos
+	fmt.Printf("helloMessage: %T\n", helloMessage)
+	fmt.Printf("cursos: %T\n", cursos)
+
+	normalFunction("Hola mundo")
+	tripleArgumento(1, 2, "hola")
+
+	value := returnValue(2)
+	fmt.Println("Value: ", value)
+
+	value1, value2 := doubleReturn(4)
+	fmt.Println("value1 y value2: ", value1, value2)
 
 }
