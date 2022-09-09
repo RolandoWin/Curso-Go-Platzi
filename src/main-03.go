50e356e0f07ca83ee8739925b69c4ce49d9aaef6
Rolando <rolandolazaro@gmail.com>
2022-09-08 23:01:58 -0500

    Array y Slices

---
+++ src/main-03.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main03() {
+	//Defer
+	defer fmt.Println("Hola")
+	fmt.Println("Mundo")
+
+	//Continue y break
+	for i := 0; i < 10; i++ {
+
+		//Continue
+		if i == 2 {
+			fmt.Println("Es 2")
+			continue
+		}
+
+		fmt.Println("i es:", i)
+
+		if i == 8 {
+			fmt.Println("Break en 8")
+			break
+		}
+	}
+}
--- src/main.go
+++ src/main.go
@@ -3,24 +3,29 @@ package main
 import "fmt"
 
 func main() {
-	//Defer
-	defer fmt.Println("Hola")
-	fmt.Println("Mundo")
+	//Array
+	var array [4]int
+	fmt.Println(array)
+	array[0] = 1
+	array[1] = 2
+	fmt.Println(array, len(array), cap(array))
 
-	//Continue y break
-	for i := 0; i < 10; i++ {
+	//Slice
+	slice := []int{0, 1, 2, 3, 4, 5, 6}
+	fmt.Println(slice, len(slice), cap(slice))
 
-		//Continue
-		if i == 2 {
-			fmt.Println("Es 2")
-			continue
-		}
+	//Método en el slice
+	fmt.Println(slice[0])
+	fmt.Println(slice[:3])
+	fmt.Println(slice[2:4])
+	fmt.Println(slice[4:])
 
-		fmt.Println("i es:", i)
+	//Append: para agregar un elemento al slice
+	slice = append(slice, 7)
+	fmt.Println(slice)
 
-		if i == 8 {
-			fmt.Println("Break en 8")
-			break
-		}
-	}
+	//Append: agregar una nueva lista
+	newSlice := []int{8, 9, 10}
+	slice = append(slice, newSlice...)
+	fmt.Println(slice)
 }
