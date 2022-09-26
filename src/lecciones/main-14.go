a0565e305a9a49502dc1afdb179345fc5182fc9f
Rolando <rolandolazaro@gmail.com>
2022-09-26 00:57:01 -0500

    Range, Close y Select en channels

---
+++ src/lecciones/main-14.go
@@ -0,0 +1,15 @@
+// Channels: La forma de organizar las goroutines
+package main
+
+import "fmt"
+
+func say(texto string, c chan<- string) {
+	c <- texto
+}
+
+func main-14() {
+	c := make(chan string, 1)
+	fmt.Println("Hello")
+	go say("Bye", c)
+	fmt.Println(<-c)
+}
--- src/main.go
+++ src/main.go
@@ -1,15 +1,42 @@
-// Channels: La forma de organizar las goroutines
+// Range, Close y Select en channels
 package main
 
 import "fmt"
 
-func say(texto string, c chan<- string) {
+func message(texto string, c chan string) {
 	c <- texto
 }
 
 func main() {
-	c := make(chan string, 1)
-	fmt.Println("Hello")
-	go say("Bye", c)
-	fmt.Println(<-c)
+	//El canal maneja dos datos a la vez
+	c := make(chan string, 2)
+	c <- "Mensaje-01"
+	c <- "Mensaje-02"
+
+	//len: cantidad de canales utilizados
+	//cap: capacidad maxima de canales
+	fmt.Println(len(c), cap(c))
+
+	//Range y Close
+	close(c)
+
+	for message := range c {
+		fmt.Println(message)
+	}
+
+	//Select
+	email1 := make(chan string)
+	email2 := make(chan string)
+
+	go message("mensaje1", email1)
+	go message("mensaje2", email2)
+
+	for i := 0; i < 2; i++ {
+		select {
+		case m1 := <-email1:
+			fmt.Println("Email recibido de email1", m1)
+		case m2 := <-email2:
+			fmt.Println("Email recibido de email2", m2)
+		}
+	}
 }
